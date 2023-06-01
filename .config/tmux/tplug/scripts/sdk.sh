da2d77c178dc0bc1ae9d56177d315f0283437253
Henry Purdum <henrypurdum@gmail.com>
2023-06-01 16:15:00 -0700

    6/1/23

--- .config/tmux/tplug/scripts/sdk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Retrieve SDK directory
-CURRENT_DIR=$(tmux show-option -gqv "@tplugPath")
-
-# Source SDK
-source "$CURRENT_DIR/scripts/tmuxwrappers.sh"
-source "$CURRENT_DIR/scripts/util.sh"
