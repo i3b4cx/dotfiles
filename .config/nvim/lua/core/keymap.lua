54e7fb140dd83e14bc57c945f0fa248c071c06bc
Henry Purdum <henrypurdum@gmail.com>
2023-06-07 10:43:43 -0700

    6/7/23

--- .config/nvim/lua/core/keymap.lua
+++ .config/nvim/lua/core/keymap.lua
@@ -11,3 +11,13 @@ vim.keymap.set("n", "fs", "<cmd>:Telescope live_grep<CR>")
 
 vim.keymap.set("n", "<leader>t", "<cmd>:ToggleTerm<CR>")
 vim.keymap.set("t", "<leader>t", "<cmd>:ToggleTerm<CR>")
+
+vim.keymap.set("n", "<C-h>", "<cmd>:wincmd h<CR>")
+vim.keymap.set("n", "<C-j>", "<cmd>:wincmd j<CR>")
+vim.keymap.set("n", "<C-k>", "<cmd>:wincmd k<CR>")
+vim.keymap.set("n", "<C-l>", "<cmd>:wincmd l<CR>")
+
+vim.keymap.set("t", "<C-h>", "<cmd>:wincmd h<CR>")
+vim.keymap.set("t", "<C-j>", "<cmd>:wincmd j<CR>")
+vim.keymap.set("t", "<C-k>", "<cmd>:wincmd k<CR>")
+vim.keymap.set("t", "<C-l>", "<cmd>:wincmd l<CR>")
