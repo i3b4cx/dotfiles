4955549e91a7be9dce1c58a5cc40b0d9680fb3ef
Henry Purdum <henrypurdum@gmail.com>
2023-06-07 16:48:56 -0700

    6/7/23

--- .config/nvim/lua/core/config.lua
+++ .config/nvim/lua/core/config.lua
@@ -26,5 +26,5 @@ vim.opt.fillchars = { eob = ' ' }
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-vim.cmd [[colorscheme gruvbox-material]]
+vim.cmd [[colorscheme kanagawa-lotus]]
 vim.cmd [[let g:neo_tree_remove_legacy_commands = 1]]
