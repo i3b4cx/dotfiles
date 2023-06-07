c80f3299a450b9820954463e2bf7b6a8c399630c
Henry Purdum <henrypurdum@gmail.com>
2023-06-07 11:26:42 -0700

    6/7/23

--- .config/nvim/lua/core/keymap.lua
+++ .config/nvim/lua/core/keymap.lua
@@ -1,13 +1,16 @@
 vim.g.mapleader = ' '
 
-vim.keymap.set("n", "r", "<cmd>:Lazy<CR>")
+vim.keymap.set("n", "l", "<cmd>:Lazy<CR>")
+vim.keymap.set("n", "br", "<cmd>:!build-release.sh<CR>")
+vim.keymap.set("n", "bd", "<cmd>:!build-debug.sh<CR>")
+vim.keymap.set("n", "bc", "<cmd>:!rm -rf build<CR>")
 
 vim.keymap.set("n", "<leader>", "<cmd>:WhichKey<CR>")
 vim.keymap.set("n", "<leader>f", "<cmd>:Neotree<CR>")
 vim.keymap.set("n", "<leader>m", "<cmd>:MinimapToggle<CR>")
 
-vim.keymap.set("n", "ff", "<cmd>:Telescope find_files<CR>")
-vim.keymap.set("n", "fs", "<cmd>:Telescope live_grep<CR>")
+vim.keymap.set("n", "<leader>ff", "<cmd>:Telescope find_files<CR>")
+vim.keymap.set("n", "<leader>fs", "<cmd>:Telescope live_grep<CR>")
 
 vim.keymap.set("n", "<leader>t", "<cmd>:ToggleTerm<CR>")
 vim.keymap.set("t", "<leader>t", "<cmd>:ToggleTerm<CR>")
