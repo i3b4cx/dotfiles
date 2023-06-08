be6d8fbe40322fefe9f0389235ae7600e6a76b23
Henry Purdum <henrypurdum@gmail.com>
2023-06-08 11:26:04 -0700

    6/7/23

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -20,7 +20,7 @@ require "core.config"
 -- specific setup
 require "bufferline".setup()
 require "gitsigns".setup()
-require "lspconfig".clangd.setup{}
+require "nvim-tree".setup()
 require "lualine".setup{
   options = {
     icons_enabled = true,
@@ -61,25 +61,3 @@ require "lualine".setup{
   inactive_winbar = {},
   extensions = {}
 }
-
--- nvim-lspconfig bindings
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
--- Use LspAttach autocommand to only map the following keys
--- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-  end,
-})
--- .config/nvim/lua/core/keymap.lua
+++ .config/nvim/lua/core/keymap.lua
@@ -4,9 +4,11 @@ vim.keymap.set("n", "l", "<cmd>:Lazy<CR>")
 vim.keymap.set("n", "br", "<cmd>:!build-release.sh<CR>")
 vim.keymap.set("n", "bd", "<cmd>:!build-debug.sh<CR>")
 vim.keymap.set("n", "bc", "<cmd>:!rm -rf build<CR>")
+vim.keymap.set("n", "f", "<cmd>:NvimTreeToggle<CR>")
 
-vim.keymap.set("n", "<leader>f", "<cmd>:Neotree<CR>")
 vim.keymap.set("n", "<leader>m", "<cmd>:MinimapToggle<CR>")
 
 vim.keymap.set("n", "<leader>ff", "<cmd>:Telescope find_files<CR>")
 vim.keymap.set("n", "<leader>fs", "<cmd>:Telescope live_grep<CR>")
+
+vim.keymap.set("n", "gd", ":call CocAction('jumpDefinition')<CR>")
