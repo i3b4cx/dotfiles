414a2e80f3781dbd5df1cc2c77af38b565d3c25c
Henry Purdum <henrypurdum@gmail.com>
2023-06-09 13:29:02 -0700

    6/9/23

--- .config/nvim/lua/core/keymap.lua
+++ .config/nvim/lua/core/keymap.lua
@@ -6,8 +6,6 @@ vim.keymap.set("n", "bd", "<cmd>:!build-debug.sh<CR>")
 vim.keymap.set("n", "bc", "<cmd>:!rm -rf build<CR>")
 vim.keymap.set("n", ";", "<cmd>:NvimTreeToggle<CR>")
 
-vim.keymap.set("n", "<leader>m", "<cmd>:MinimapToggle<CR>")
-
 vim.keymap.set("n", "<leader>ff", "<cmd>:Telescope find_files<CR>")
 vim.keymap.set("n", "<leader>fs", "<cmd>:Telescope live_grep<CR>")
 
