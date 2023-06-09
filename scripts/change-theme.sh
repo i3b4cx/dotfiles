a4988c08187039c1a40f755a5d7aa4a06c232f57
Henry Purdum <henrypurdum@gmail.com>
2023-06-09 16:30:16 -0700

    6/9/23

--- scripts/change-theme.sh
+++ scripts/change-theme.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-list=""
-theme=$1
-
-case $theme in
+# change theme if valid
+case $1 in
 kanagawa|kanagawa-wave|kanagawa-dragon|kanagawa-lotus|horizon)
     sed -i.bak -e "s/.*colorscheme.*/vim.cmd [[colorscheme $theme]]/g" ~/.config/nvim/lua/core/options.lua
     sed -i.bak -e "s/.*include.*/include $1.conf/g" ~/.config/kitty/kitty.conf
