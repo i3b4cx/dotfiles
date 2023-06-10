da825c53cceeb6e72e7123a47a6c5979b723fb65
Henry Purdum <henrypurdum@gmail.com>
2023-06-09 18:40:00 -0700

    6/9/23

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,11 +1,4 @@
 return {
-    "sainnhe/everforest",
-    "sainnhe/gruvbox-material",
-    "projekt0n/github-nvim-theme",
     "lunarvim/horizon.nvim",
     "rebelot/kanagawa.nvim",
-    "loctvl842/monokai-pro.nvim",
-    config = function()
-        require("monokai-pro").setup()
-    end
 }
