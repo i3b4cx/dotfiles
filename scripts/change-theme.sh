e61db06c37b7528d6116ddf188abd3b444f0828e
Henry Purdum <henrypurdum@gmail.com>
2023-06-13 16:07:06 -0700

    forgot to add gruvbox to change theme script in last commit

--- scripts/change-theme.sh
+++ scripts/change-theme.sh
@@ -2,7 +2,7 @@
 
 # change theme if valid
 case $1 in
-kanagawa|kanagawa-wave|kanagawa-dragon|kanagawa-lotus|horizon)
+kanagawa|kanagawa-wave|kanagawa-dragon|kanagawa-lotus|horizon|gruvbox)
     sed -i.bak -e "s/.*colorscheme.*/vim.cmd [[colorscheme $1]]/g" ~/.config/nvim/lua/core/options.lua
     sed -i.bak -e "s/.*include.*/include $1.conf/g" ~/.config/kitty/kitty.conf
     ;;
