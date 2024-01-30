c6f3ff58be18d81b784c3a4fa52892d28f1b8fb7
Henry Purdum <henrypurdum@gmail.com>
2024-01-29 19:54:45 -0800

    adding nvChad config

---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,20 @@
+local plugins = {
+
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    opts = {
+      -- configurations go here
+    },
+    init = function()
+      require("core.utils").lazy_load "barbecue"
+    end
+  }
+}
+
+return plugins
